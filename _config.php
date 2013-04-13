2f65dc939ac1cfcd0162b946ae336817b09051ed
Jason Irish <jirish@dynamicdoes.com>
2013-04-13 13:28:15 -0500

    moved MiniCart config to mysite/_config.php

--- _config.php
+++ _config.php
@@ -3,10 +3,5 @@
 HtmlEditorConfig::get('cms')->setOption('theme_advanced_styles', 'highlight=highlight;no-border=no-border,break=break');
 
 // Share Icons
-//Object::add_extension('CartProduct', 'ShareExtension');
+if (class_exists('ShareExtension')) Object::add_extension('CartProduct', 'ShareExtension');
 
-// Shopping Cart
-MiniCart::set_business_email("info@dynamicdevelop.com");
-MiniCart::set_currency_code("USD");
-MiniCart::set_return_url("https://webbuilder.local:8888/page?success");
-MiniCart::set_cancel_return_url("https://webbuilder.local:8888/page?cancel");
