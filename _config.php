8529b7da29fb5001638601e7a155186076e01832
Jason Irish <jsirish@users.noreply.github.com>
2019-07-01 21:30:16 -0500

    Update _config.php
    remove references to `Object`

--- _config.php
+++ _config.php
@@ -3,5 +3,5 @@
 HtmlEditorConfig::get('cms')->setOption('theme_advanced_styles', 'highlight=highlight;no-border=no-border,break=break');
 
 // Share Icons
-if (class_exists('ShareExtension')) Object::add_extension('CartProduct', 'ShareExtension');
+if (class_exists('ShareExtension')) CartProduct::add_extension('ShareExtension');
 
